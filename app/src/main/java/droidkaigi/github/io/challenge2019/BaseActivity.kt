c2f861bea86a26d03d70747172e81f1d761516be
Tomoya Miwa <tomoyann@gmail.com>
2018-12-29 20:03:23 +0900

    Use HackerNewsRepository in MainActivity

--- app/src/main/java/droidkaigi/github/io/challenge2019/BaseActivity.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/BaseActivity.kt
@@ -9,6 +9,8 @@ import android.view.MenuItem
 import android.widget.Toast
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.*
 
 abstract class BaseActivity : AppCompatActivity() {
