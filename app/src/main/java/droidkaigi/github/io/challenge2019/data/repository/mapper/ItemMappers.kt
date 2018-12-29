281b8eed38b06340193b7ff16ae31f3a64a8ff88
Tomoya Miwa <tomoyann@gmail.com>
2018-12-29 16:39:23 +0900

    Add ItemMappers
    Item -> Story
    Item -> Comment

---
+++ app/src/main/java/droidkaigi/github/io/challenge2019/data/repository/mapper/ItemMappers.kt
@@ -0,0 +1,29 @@
+package droidkaigi.github.io.challenge2019.data.repository.mapper
+
+import droidkaigi.github.io.challenge2019.data.api.response.Item
+import droidkaigi.github.io.challenge2019.data.repository.entity.Comment
+import droidkaigi.github.io.challenge2019.data.repository.entity.Story
+import java.util.*
+
+fun Item.toStory(
+    comments: List<Comment>
+): Story = Story(
+    author = author,
+    descendants = descendants,
+    id = id,
+    comments = comments,
+    score = score,
+    time = Date(time * 1000),
+    title = title,
+    url = url
+)
+
+fun Item.toComment(
+    comments: List<Comment>
+): Comment = Comment(
+    author = author,
+    id = id,
+    comments = comments,
+    text = text ?: "",
+    time = Date(time * 1000)
+)
