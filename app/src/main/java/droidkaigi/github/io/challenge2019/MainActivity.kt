55567a45ea3824d78f15a04d5d38578c86ce4b6b
Tomoya Miwa <tomoyann@gmail.com>
2018-12-29 22:21:10 +0900

    Use ViewModel in MainActivity

--- app/src/main/java/droidkaigi/github/io/challenge2019/MainActivity.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/MainActivity.kt
@@ -1,33 +1,24 @@
 package droidkaigi.github.io.challenge2019
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
 import com.squareup.moshi.Types
-import droidkaigi.github.io.challenge2019.data.api.HackerNewsApi
 import droidkaigi.github.io.challenge2019.data.db.ArticlePreferences
 import droidkaigi.github.io.challenge2019.data.db.ArticlePreferences.Companion.saveArticleIds
-import droidkaigi.github.io.challenge2019.data.repository.HackerNewsRepository
 import droidkaigi.github.io.challenge2019.data.repository.Resource
 import droidkaigi.github.io.challenge2019.data.repository.entity.Story
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.CountDownLatch
 
 class MainActivity : BaseActivity() {
 
@@ -45,6 +36,10 @@ class MainActivity : BaseActivity() {
     private val storiesJsonAdapter =
         moshi.adapter<List<Story?>>(Types.newParameterizedType(List::class.java, Story::class.java))
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
     override fun getContentView(): Int {
         return R.layout.activity_main
     }
@@ -55,31 +50,7 @@ class MainActivity : BaseActivity() {
         progressView = findViewById(R.id.progress)
         swipeRefreshLayout = findViewById(R.id.swipe_refresh)
 
-        val itemDecoration = DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
-        recyclerView.addItemDecoration(itemDecoration)
-        storyAdapter = StoryAdapter(
-            stories = mutableListOf(),
-            onClickStory = { story ->
-                val storyJson = storyJsonAdapter.toJson(story)
-                val intent = Intent(this@MainActivity, StoryActivity::class.java).apply {
-                    putExtra(StoryActivity.EXTRA_ITEM_JSON, storyJson)
-                }
-                startActivityForResult(intent)
-            },
-            onClickMenuItem = { story, menuItemId ->
-                when (menuItemId) {
-                    R.id.copy_url -> {
-                        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                        clipboard.primaryClip = ClipData.newPlainText("url", story.url)
-                    }
-                    R.id.refresh -> {
-                        reloadStory(story.id)
-                    }
-                }
-            },
-            alreadyReadStories = ArticlePreferences.getArticleIds(this)
-        )
-        recyclerView.adapter = storyAdapter
+        setupRecyclerView()
 
         swipeRefreshLayout.setOnRefreshListener { loadTopStories() }
 
@@ -96,37 +67,22 @@ class MainActivity : BaseActivity() {
             return
         }
 
-        loadTopStories(true)
-    }
-
-    private fun loadTopStories(showProgressView: Boolean = false) {
-        val liveTopStories = HackerNewsRepository.getTopStories()
-        liveTopStories.observe(this, Observer { resource ->
+        viewModel.topStories.observe(this, Observer { resource ->
             when (resource) {
-                is Resource.Loading -> {
-                    if (showProgressView) progressView.visibility = Util.setVisibility(true)
-                }
                 is Resource.Success -> {
                     progressView.visibility = View.GONE
                     swipeRefreshLayout.isRefreshing = false
                     storyAdapter.stories = resource.data.toMutableList()
                     storyAdapter.alreadyReadStories = ArticlePreferences.getArticleIds(this@MainActivity)
                     storyAdapter.notifyDataSetChanged()
-                    liveTopStories.removeObservers(this)
                 }
                 is Resource.Error -> {
                     showError(resource.t)
-                    liveTopStories.removeObservers(this)
                 }
             }
         })
-    }
 
-    private fun reloadStory(id: Long) {
-        if (storyAdapter.stories.none { it?.id == id }) return
-
-        val liveStory = HackerNewsRepository.getStory(id)
-        liveStory.observe(this, Observer { resource ->
+        viewModel.story.observe(this, Observer { resource ->
             when (resource) {
                 is Resource.Success -> {
                     val story = resource.data
@@ -138,14 +94,47 @@ class MainActivity : BaseActivity() {
                         storyAdapter.alreadyReadStories = ArticlePreferences.getArticleIds(this@MainActivity)
                         storyAdapter.notifyItemChanged(index)
                     }
-                    liveStory.removeObservers(this)
                 }
                 is Resource.Error -> {
                     showError(resource.t)
-                    liveStory.removeObservers(this)
                 }
             }
         })
+
+        loadTopStories(true)
+    }
+
+    private fun setupRecyclerView() {
+        val itemDecoration = DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
+        recyclerView.addItemDecoration(itemDecoration)
+        storyAdapter = StoryAdapter(
+            stories = mutableListOf(),
+            onClickStory = { story ->
+                val storyJson = storyJsonAdapter.toJson(story)
+                val intent = Intent(this@MainActivity, StoryActivity::class.java).apply {
+                    putExtra(StoryActivity.EXTRA_ITEM_JSON, storyJson)
+                }
+                startActivity(intent)
+            },
+            onClickMenuItem = { story, menuItemId ->
+                when (menuItemId) {
+                    R.id.copy_url -> {
+                        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                        clipboard.primaryClip = ClipData.newPlainText("url", story.url)
+                    }
+                    R.id.refresh -> {
+                        viewModel.loadStory(story.id)
+                    }
+                }
+            },
+            alreadyReadStories = ArticlePreferences.getArticleIds(this)
+        )
+        recyclerView.adapter = storyAdapter
+    }
+
+    private fun loadTopStories(showProgressView: Boolean = false) {
+        if (showProgressView) progressView.visibility = Util.setVisibility(true)
+        viewModel.loadTopStories()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
