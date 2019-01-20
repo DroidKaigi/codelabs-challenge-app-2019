53460e65f93b23ddd8cf5d0bf92b3394ae8aea4e
shinmiy <shinmiyaccs@gmail.com>
2019-01-21 03:49:11 +0900

    Add generous hints to TODO 4

--- app/src/main/java/droidkaigi/github/io/challenge2019/StoryViewModel.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/StoryViewModel.kt
@@ -11,19 +11,34 @@ class StoryViewModel: ViewModel() {
     // TODO: 4. Implement StoryViewModel
     // refer: https://developer.android.com/topic/libraries/architecture/livedata#transform_livedata
 
+    // private val storyInput = MutableLiveData<Story>()
     val comments: LiveData<Resource<List<Comment?>>> =
         TODO("Implement this property")
+        // Transformations.switchMap(storyInput) { story ->
+        //    HackerNewsRepository.getComments(story)
+        // }
 
+    // private val isWebPageLoadingInput = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> =
         TODO("Implement this property")
+        // MediatorLiveData<Boolean>().apply {
+        //     addSource(comments) { resource ->
+        //         value = resource is Resource.Loading || isWebPageLoadingInput.value ?: false
+        //     }
+        //     addSource(isWebPageLoadingInput) { isWebPageLoading ->
+        //         value = isWebPageLoading ?: false || comments.value is Resource.Loading
+        //     }
+        //  }
 
 
     fun loadComments(story: Story) = {
         TODO("Implement this method")
+        // storyInput.postValue(story)
     }
 
     fun onStartWebPageLoading() {
         TODO("Implement this method")
+        // isWebPageLoadingInput.postValue(true)
     }
 
     fun onFinishWebPageLoading() {
