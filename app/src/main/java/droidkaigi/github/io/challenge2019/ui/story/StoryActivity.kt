e279c93fb419afa71522d0bb50b5c09371330e76
Kenji Abe <kenji.01.star@gmail.com>
2019-02-08 15:35:17 +0900

    Organize story

--- app/src/main/java/droidkaigi/github/io/challenge2019/ui/story/StoryActivity.kt
+++ app/src/main/java/droidkaigi/github/io/challenge2019/ui/story/StoryActivity.kt
@@ -9,16 +9,14 @@ import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.ProgressBar
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.squareup.moshi.Types
 import droidkaigi.github.io.challenge2019.BaseActivity
 import droidkaigi.github.io.challenge2019.R
-import droidkaigi.github.io.challenge2019.Util
-import droidkaigi.github.io.challenge2019.core.data.api.HackerNewsApi
 import droidkaigi.github.io.challenge2019.core.data.api.response.Item
 import droidkaigi.github.io.challenge2019.databinding.ActivityStoryBinding
 import droidkaigi.github.io.challenge2019.di.component
@@ -39,12 +37,7 @@ class StoryActivity : BaseActivity() {
         ViewModelProviders.of(this, viewModelFactory).get(StoryViewModel::class.java)
     }
 
-    private lateinit var webView: WebView
-    private lateinit var recyclerView: androidx.recyclerview.widget.RecyclerView
-    private lateinit var progressView: ProgressBar
-
     private lateinit var commentAdapter: CommentAdapter
-    private lateinit var hackerNewsApi: HackerNewsApi
 
     private val itemJsonAdapter = moshi.adapter(Item::class.java)
     private val itemsJsonAdapter =
@@ -65,21 +58,10 @@ class StoryActivity : BaseActivity() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        webView = findViewById(R.id.web_view)
-        recyclerView = findViewById(R.id.comment_recycler)
-
-        val retrofit = createRetrofit("https://hacker-news.firebaseio.com/v0/")
-
-        hackerNewsApi = retrofit.create(HackerNewsApi::class.java)
-
-        recyclerView.isNestedScrollingEnabled = false
-        val itemDecoration = androidx.recyclerview.widget.DividerItemDecoration(
-            recyclerView.context,
-            androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
-        )
-        recyclerView.addItemDecoration(itemDecoration)
+        binding.commentRecycler.isNestedScrollingEnabled = false
+        binding.commentRecycler.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
         commentAdapter = CommentAdapter(emptyList())
-        recyclerView.adapter = commentAdapter
+        binding.commentRecycler.adapter = commentAdapter
 
         val savedComments = savedInstanceState?.let { bundle ->
             bundle.getString(STATE_COMMENTS)?.let { itemsJson ->
@@ -90,7 +72,7 @@ class StoryActivity : BaseActivity() {
         if (savedComments != null) {
             commentAdapter.comments = savedComments
             commentAdapter.notifyDataSetChanged()
-            webView.loadUrl(item.url)
+            loadUrl()
             return
         }
 
@@ -104,7 +86,7 @@ class StoryActivity : BaseActivity() {
     }
 
     private fun loadUrl() {
-        webView.webViewClient = object : WebViewClient() {
+        binding.webView.webViewClient = object : WebViewClient() {
 
             override fun onPageFinished(view: WebView?, url: String?) {
                 viewModel.isWebLoading.value = false
@@ -116,13 +98,12 @@ class StoryActivity : BaseActivity() {
         }
 
         viewModel.isWebLoading.value = true
-        webView.loadUrl(item.url)
+        binding.webView.loadUrl(item.url)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
             R.id.refresh -> {
-                progressView.visibility = Util.setVisibility(true)
                 loadUrl()
                 viewModel.getComments(this.item)
                 return true
